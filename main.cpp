bf9b5f78b19a6e6f471abb77558b2625830bd48d
aMbZfcaYn <mengzy2024@shanghaitech.edu.cn>
2026-04-22 00:19:41 +0800

    feat: Full-colored triangle & Wireframe triangle

--- main.cpp
+++ main.cpp
@@ -52,11 +52,11 @@ double signed_triangle_area (int ax, int ay, int bx, int by, int cx, int cy)
     return 0.5 * ((by * ax - ay * bx) + (cy * bx - by * cx) + (ay * cx - cy * ax));
 }
 
-void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuffer, TGAColor color) 
+void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuffer, TGAColor a_color, TGAColor b_color, TGAColor c_color) 
 {
-    line(ax, ay, bx, by, framebuffer, color);
-    line(bx, by, cx, cy, framebuffer, color);
-    line(cx, cy, ax, ay, framebuffer, color);
+    // line(ax, ay, bx, by, framebuffer, color);
+    // line(bx, by, cx, cy, framebuffer, color);
+    // line(cx, cy, ax, ay, framebuffer, color);
 
     // Calculate the bouding box:
     int boundMinX = std::min(std::min(ax, bx), cx);
@@ -75,7 +75,16 @@ void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuf
             double gamma = signed_triangle_area(x, y, ax, ay, bx, by) / total_area;
             if (alpha < 0 || beta < 0 || gamma < 0)
                 continue;
-            framebuffer.set(x, y, color);
+            double frac = alpha + beta + gamma;
+            double max_weight = std::max(std::max(alpha, beta), gamma);
+            double min_weight = std::min(std::min(alpha, beta), gamma);
+            uint8_t B = (alpha * a_color[0] + beta * b_color[0] + gamma * c_color[0]) / frac;
+            uint8_t G = (alpha * a_color[1] + beta * b_color[1] + gamma * c_color[1]) / frac;
+            uint8_t R = (alpha * a_color[2] + beta * b_color[2] + gamma * c_color[2]) / frac;
+            uint8_t A = (alpha * a_color[3] + beta * b_color[3] + gamma * c_color[3]) / frac;
+            TGAColor color = {B, G, R, A};
+            if (min_weight < 0.1)
+                framebuffer.set(x, y, color);
         }
 }
 
@@ -85,9 +94,11 @@ int main (int argc, char** argv)
     constexpr int height = 200;
     TGAImage framebuffer(width, height, TGAImage::RGB);
 
-    triangle(  7, 45, 35, 100, 45,  60, framebuffer, red);
-    triangle(120, 35, 90,   5, 45, 110, framebuffer, white);
-    triangle(115, 83, 80,  90, 85, 120, framebuffer, green);
+    // triangle(  7, 45, 35, 100, 45,  60, framebuffer, red);
+    // triangle(120, 35, 90,   5, 45, 110, framebuffer, white);
+    // triangle(115, 83, 80,  90, 85, 120, framebuffer, green);
+
+    triangle(0, 0, 150, 50, 50, 160, framebuffer, red, green, blue);
 
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
