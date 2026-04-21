dac65fb363faa149b2491fca7b070ba135122871
aMbZfcaYn <mengzy2024@shanghaitech.edu.cn>
2026-04-21 23:37:31 +0800

    feat: Triangle Scanline Filling

--- main.cpp
+++ main.cpp
@@ -9,67 +9,85 @@ constexpr TGAColor red     = {  0,   0, 255, 255};
 constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
-void Line (int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
+void line (int ax, int ay, int bx, int by, TGAImage& image, TGAColor color)
 {
     bool swapped = false;
-    if (std::abs(x0 - x1) < std::abs(y0 - y1))
+    if (std::abs(ax - bx) < std::abs(ay - by))
     {
-        std::swap(x0, y0);
-        std::swap(x1, y1);
+        std::swap(ax, ay);
+        std::swap(bx, by);
         swapped = true;
     }
 
-    if (x0 > x1)
+    if (ax > bx)
     {
-        std::swap(x0, x1);
-        std::swap(y0, y1);
+        std::swap(ax, bx);
+        std::swap(ay, by);
     }
 
-    int dx = x1 - x0;
-    int dy = y1 - y0;
-    float derror = std::abs(dy / float(dx));
-    float error = 0;
-    int y = y0;
+    int dx = bx - ax;
+    int dy = by - ay;
+    int ierror = 0;
+    int y = ay;
 
-    for (int x = x0; x <= x1; x++)
+    for (int x = ax; x <= bx; x++)
     {
         if (swapped)
             image.set(y, x, color);
         else
             image.set(x, y, color);
 
-        error += derror;
-        if (error > .5)
+        ierror += 2 * std::abs(dy);
+        if (ierror > dx)
         {
             y += (dy > 0 ? 1 : -1);
-            error -= 1;
+            ierror -= 2 * dx;
+        }
+    }
+}
+
+void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuffer, TGAColor color) 
+{
+    line(ax, ay, bx, by, framebuffer, color);
+    line(bx, by, cx, cy, framebuffer, color);
+    line(cx, cy, ax, ay, framebuffer, color);
+
+    // Sort the vertices int x direction:
+    if (ax > bx) { std::swap(ax, bx); std::swap(ay, by); }
+    if (ax > cx) { std::swap(ax, cx); std::swap(ay, cy); }
+    if (bx > cx) { std::swap(bx, cx); std::swap(by, cy); }
+
+    // Step from left to right:
+    float ac_slope = (cy - ay) / (float)(cx - ax);
+    float ab_slope = (by - ay) / (float)(bx - ax);
+    float bc_slope = (cy - by) / (float)(cx - bx);
+
+    for (int x = ax + 1; x < cx; x++)
+    {
+        if (x <= bx)
+        {
+            int ab_y = std::floor(ab_slope * (x - ax) + ay);
+            int ac_y = std::floor(ac_slope * (x - ax) + ay);
+            line(x, ab_y, x, ac_y, framebuffer, color);
+        }
+        else
+        {
+            int bc_y = std::floor(bc_slope * (x - bx) + by);
+            int ac_y = std::floor(ac_slope * (x - ax) + ay);
+            line(x, bc_y, x, ac_y, framebuffer, color);
         }
     }
 }
 
 int main (int argc, char** argv) 
 {
-    constexpr int width  = 1000;
-    constexpr int height = 1000;
+    constexpr int width  = 200;
+    constexpr int height = 200;
     TGAImage framebuffer(width, height, TGAImage::RGB);
 
-    Model model{"obj/african_head/african_head.obj"};
-
-    for (int i = 0; i < model.nfaces(); i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            vec4 v0 = model.vert(i, j);
-            vec4 v1 = model.vert(i, (j + 1) % 3);
-
-            int x0 = (v0.x + 1.) * width / 2.;
-            int y0 = (v0.y + 1.) * height / 2.;
-            int x1 = (v1.x + 1.) * width / 2.;
-            int y1 = (v1.y + 1.) * height / 2.;
-
-            Line(x0, y0, x1, y1, framebuffer, yellow);
-        }
-    }
+    triangle(  7, 45, 35, 100, 45,  60, framebuffer, red);
+    triangle(120, 35, 90,   5, 45, 110, framebuffer, white);
+    triangle(115, 83, 80,  90, 85, 120, framebuffer, green);
 
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
